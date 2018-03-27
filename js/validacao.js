280d42dab587cca11c1576313b48c9c0dda0616a
Kacio Oliveira <kaciowiturino@hotmail.com>
2018-03-26 23:02:36 -0300

    Ajuste JS

--- js/validacao.js
+++ js/validacao.js
@@ -1,42 +1,62 @@
 function validaForm(event){
 	event.preventDefault();
-	console.log("evento ok");
 	
 	var span_nome = document.querySelector("#erro-nome");
 	var span_email = document.querySelector("#erro-email");
 	var span_empresa = document.querySelector("#erro-empresa");	
 
+	
+
 	var nome = document.querySelector("#nome");
 	var email = document.querySelector("#email");
 	var empresa = document.querySelector("#empresa");
-	if(nome.value==""){
-		span_nome.textContent = "Por favor digite o seu nome Completo";
+
+	var nomeValido=validaDados(nome,span_nome);
+	var emailValido=validaDados(email,span_email);
+	var empresaValida=validaDados(empresa,span_empresa);
+
+	var jsonDadosValidados={
+		nome : nomeValido,
+		email : emailValido,
+		empresa : empresaValida
+	}
+
+
+
+	enviaDados(JSON.stringify(jsonDadosValidados));
+		
+	
+}
+
+
+function enviaDados(dadosForm){
+	function reqListener () {
+  		console.log(this.responseText);
+	};
+
+	let httpRequest = new XMLHttpRequest();
+
+	let url = "https://my-json-server.typicode.com/typicode/demo/posts"; 
+	httpRequest.onload = reqListener;
+	httpRequest.open("post", url, true);
+	httpRequest.setRequestHeader("Content-Type", "application/json");
+	httpRequest.send(dadosForm);
+}
+
+function validaDados(elemento,span){
+	
+	
+
+	
+		if(elemento.value==""){
+		span.textContent =`Por favor digite o seu ${elemento.id} Completo`;
 		nome.focus();
-		return;
+		return false;
+		
 	}else{
-		span_nome.textContent = "";
+
+		span.textContent = "";
+		return elemento.value;
 	}
 
-	if(email.value == ""){
-		span_email.textContent = "Por favor digite o seu email";
-		email.focus();
-		return;
-	}else{
-		span_email.textContent="";	
-	}
-	
-	if(empresa.value=="")	{
-		span_empresa.textContent = "Por favor digite o nome da empresa";
-		email.focus();
-		return;	
-	}else{
-		span_empresa.textContent="";
-	}
-	
-		
-		
-		
-	
-
-	
 }
